aa0c3fce3c77f858aa67f464d0392063598a11d1
Jon Penwood <jonp@polarity.io>
2022-02-08 13:03:17 -0600

    Added hash entity type

--- config/config.js
+++ config/config.js
@@ -35,7 +35,7 @@ module.exports = {
     //     /(?:\w+(?:(?:\[:\]\/\/)|(?::\/\/)|(?:\[:\/\/\])))?(?:\w+\.|(?:\w+\[\.\]))+\w+(?:\/(?:[\/=\.\[\]\w&#@$%?-])*)?/
     // }
   ],
-  entityTypes: ['domain', 'email', 'IPv4', 'IPv6', 'url'],
+  entityTypes: ['domain', 'email', 'IPv4', 'IPv6', 'url', 'hash'],
   defaultColor: 'light-blue',
   logging: {
     level: 'info', //trace, debug, info, warn, error, fatal
